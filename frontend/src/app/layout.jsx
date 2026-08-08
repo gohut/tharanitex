c3d7261d6f9095db9b22bf0c48016ae0824e125f
codingharsha <harshavarthanoff@gmail.com>
2026-08-08 23:31:19 +0530

    Loading Screen Updated

---
+++ frontend/src/app/layout.jsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+  return (
+    <div className="home-loading">
+      <img
+        src="/assets/logo/logo.png"
+        alt="Tharani Textiles"
+        className="home-loading-logo"
+      />
+    </div>
+  );
+}
