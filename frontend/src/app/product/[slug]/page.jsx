de56e7f2cbffbd6421651c8a0e75898c62ecfdcc
codingharsha <harshavarthanoff@gmail.com>
2026-08-13 23:48:32 +0530

    product_card_size

--- frontend/src/app/product/[slug]/page.jsx
+++ frontend/src/app/product/[slug]/page.jsx
@@ -25,15 +25,41 @@ export default async function ProductPage({ params }) {
       <Navbar />
 
       <main className="bg-[#FBF5EA]">
-        <section className="mx-auto w-full max-w-[430px] px-4 pb-0 pt-4 sm:px-5 sm:pb-14 sm:pt-5 md:px-8 lg:max-w-[1420px] lg:px-10 lg:pt-6">
-
-          <div className="grid gap-7 sm:gap-8 lg:grid-cols-[minmax(0,720px)_minmax(320px,1fr)] lg:gap-10 xl:gap-14">
+        <section
+          className="
+            mx-auto
+            w-full
+            max-w-[430px]
+            px-0
+            pb-0
+            pt-0
+            sm:px-5
+            sm:pb-14
+            sm:pt-5
+            md:px-8
+            lg:max-w-[1420px]
+            lg:px-10
+            lg:pt-6
+          "
+        >
+          <div
+            className="
+              grid
+              gap-7
+              sm:gap-8
+              lg:grid-cols-[minmax(0,720px)_minmax(320px,1fr)]
+              lg:gap-10
+              xl:gap-14
+            "
+          >
             <ProductGallery images={product.images} />
+
             <ProductDetails product={product} />
           </div>
         </section>
 
         <RelatedProducts products={productData.relatedProducts} />
+
         <ReviewSection reviews={productData.reviews} />
       </main>
     </>
