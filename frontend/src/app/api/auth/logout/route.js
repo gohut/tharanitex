5b3bbc3029a9f4866f8b9f31a442227e4c466280
codingharsha <harshavarthanoff@gmail.com>
2026-08-23 02:34:41 +0530

    fix

--- frontend/src/app/api/auth/logout/route.js
+++ frontend/src/app/api/auth/logout/route.js
@@ -1,7 +1,5 @@
 import { AuthController } from "@/controllers/AuthController";
 
-export const runtime = "edge";
-
 export async function POST(request) {
   return await AuthController.logout(request);
 }
